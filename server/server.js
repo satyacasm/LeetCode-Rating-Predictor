b1d9a2e91f6ef542d71478e4ee4731ba285371d4
satyacasm <202151141@iiitvadodara.ac.in>
2023-05-20 13:37:37 +0530

    Tried using leetcode api🎉

--- server/server.js
+++ server/server.js
@@ -24,7 +24,43 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
 .catch((err)=>console.log("Error in connection to MongoDB"))
 
 const adminRoutes = require('./routes/adminRoutes');
+app.get('/contestRank',async (req,res)=>{
+  try {
+    const pageSize = 25;
+
+    const firstPageResponse = await axios.get(`https://leetcode.com/contest/api/ranking/weekly-contest-344/?pagination=1&region=global`);
+    const firstPageData = firstPageResponse.data;
+
+    const totalPages = Math.ceil(firstPageData.user_num / pageSize);
+
+    const participants = [];
+
+    for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+      console.log(currentPage)
+      const pageResponse = await axios.get(`https://leetcode.com/contest/api/ranking/weekly-contest-344/?pagination=${currentPage}&region=global`);
+      const pageData = pageResponse.data;
+
+      if (pageData && pageData.total_rank && Array.isArray(pageData.total_rank)) {
+        pageData.total_rank.forEach((participant) => {
+          const { username, rank } = participant;
+          participants.push({ username, rank });
+        });
+      }
+    }
+
+    res.send(participants);
+  } catch (error) {
+    console.error('Error fetching contest data:', error);
+    throw error;
+  }
+})
 app.use('/admin',adminRoutes);
+
+async function getParticipantsData() {
+  
+}
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
